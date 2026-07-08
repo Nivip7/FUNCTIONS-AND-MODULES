97c3773d09ef2723a24a94e6f9d9de0b3a4d1de5
Nivip7 <hemapandi2005_bcs29@mepcoeng.ac.in>
2026-07-09 01:00:31 +0530

    Create st_module.py

---
+++ st_module.py
@@ -0,0 +1,8 @@
+def union(a,b):
+    return a.union(b)
+def intersection(a,b):
+    return a.intersection(b)
+def difference(a,b):
+    return a.difference(b)
+def symmetric_difference(a,b):
+    return a.symmetric_difference(b)
